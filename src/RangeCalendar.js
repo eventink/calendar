15aa761596e59ae8c8bedf6d51f4cd7794b59f11
Christian Rank <christian.rank@eventinc.de>
2018-04-27 12:49:26 +0200

    feat: select multiple dates

--- src/RangeCalendar.js
+++ src/RangeCalendar.js
@@ -733,7 +733,7 @@ const RangeCalendar = createReactClass({
                   <TodayButton
                     {...props}
                     disabled={isTodayInView}
-                    value={state.value[0]}
+                    displayedValue={state.value[0]}
                     onToday={this.onToday}
                     text={locale.backToToday}
                   />
